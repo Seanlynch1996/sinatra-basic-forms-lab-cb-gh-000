6b0a3b7ed31d33888556867fa6429315c8f5fc09
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-09 23:53:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,13 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/display' do
+    @name = params[:name].to_s
+    @breed = params[:breed].to_s
+    @age = params[:age]
+
+    erb :display_puppy
+  end
+    
 end
